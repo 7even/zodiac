d33e4466bd66cb915897fd23b554d46b88fb2ffb
Vsevolod Romashov <7@7vn.ru>
2011-04-16 22:01:28 +0400

    fixed locale paths

--- lib/zodiac.rb
+++ lib/zodiac.rb
@@ -1,6 +1,6 @@
 require 'funtimes'
 require 'i18n'
-I18n.config.load_path += Dir.glob('lib/locales/*.yml')
+I18n.config.load_path += Dir.glob(File.dirname(File.expand_path(__FILE__)) + '/locales/*.yml')
 
 require 'zodiac/finder'
 require 'zodiac/date'
