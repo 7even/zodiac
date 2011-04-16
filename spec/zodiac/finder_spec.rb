a0ce6ec9571a61e28aaf825f4f8aa6688baab60f
Vsevolod Romashov <7@7vn.ru>
2011-04-16 15:46:03 +0400

    moved all functionality to Zodiac::Finder

---
+++ spec/zodiac/finder_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+module Zodiac
+  describe Finder do
+    it "generates corect dates" do
+      Finder.date_for(9, 27).should == DateTime.new(Finder::YEAR, 9, 27)
+    end
+    
+    it "returns correct zodiac sign" do
+      Finder.sign_for(:month => 9, :day => 27).should == :libra
+    end
+  end
+end
