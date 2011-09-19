3f7aeb7ff26cb02d44eaecb536ce50552b906dd8
Vsevolod Romashov <7@7vn.ru>
2011-09-20 00:24:03 +0400

    Zodiac::Finder refactoring

--- lib/zodiac/finder.rb
+++ lib/zodiac/finder.rb
@@ -2,32 +2,35 @@ module Zodiac
   module Finder
     YEAR = 2011
     
-    def self.date_for(month, day)
-      DateTime.new(YEAR, month, day)
-    end
-    
-    def self.range_for(month_start, day_start, month_end, day_end)
-      SimpleRange.new(date_for(month_start, day_start), date_for(month_end, day_end))
+    class << self
+      def date_for(month, day)
+        DateTime.new(YEAR, month, day)
+      end
+      
+      def range_for(month_start, day_start, month_end, day_end)
+        start, ending = date_for(month_start, day_start), date_for(month_end, day_end)
+        SimpleRange.new(start, ending)
+      end
     end
     
     RANGES = {
-      range_for(1,  1,  1,  20) => :capricorn,
-      range_for(1,  21, 2,  19) => :aquarius,
-      range_for(2,  20, 3,  20) => :pisces,
-      range_for(3,  21, 4,  20) => :aries,
-      range_for(4,  21, 5,  21) => :taurus,
-      range_for(5,  22, 6,  21) => :gemini,
-      range_for(6,  22, 7,  22) => :cancer,
-      range_for(7,  23, 8,  21) => :leo,
-      range_for(8,  22, 9,  23) => :virgo,
-      range_for(9,  24, 10, 23) => :libra,
-      range_for(10, 24, 11, 22) => :scorpio,
-      range_for(11, 23, 12, 22) => :sagittarius,
-      range_for(12, 23, 12, 31) => :capricorn
+      :capricorn    => range_for(1,  1,  1,  20),
+      :aquarius     => range_for(1,  21, 2,  19),
+      :pisces       => range_for(2,  20, 3,  20),
+      :aries        => range_for(3,  21, 4,  20),
+      :taurus       => range_for(4,  21, 5,  21),
+      :gemini       => range_for(5,  22, 6,  21),
+      :cancer       => range_for(6,  22, 7,  22),
+      :leo          => range_for(7,  23, 8,  21),
+      :virgo        => range_for(8,  22, 9,  23),
+      :libra        => range_for(9,  24, 10, 23),
+      :scorpio      => range_for(10, 24, 11, 22),
+      :sagittarius  => range_for(11, 23, 12, 22),
+      :capricorn    => range_for(12, 23, 12, 31)
     }
     
     def self.sign_for(date)
-      RANGES.each do |range, sign|
+      RANGES.each do |sign, range|
         if range.days.include? date_for(date[:month], date[:day])
           return I18n.t!("zodiac.#{sign}")
         end
--- spec/zodiac/finder_spec.rb
+++ spec/zodiac/finder_spec.rb
@@ -1,13 +1,15 @@
 require 'spec_helper'
 
-module Zodiac
-  describe Finder do
-    it "generates corect dates" do
-      Finder.date_for(9, 27).should == DateTime.new(Finder::YEAR, 9, 27)
+describe Zodiac::Finder do
+  describe ".date_for" do
+    it "generates correct dates" do
+      subject.date_for(9, 27).should == DateTime.new(subject::YEAR, 9, 27)
     end
-    
+  end
+  
+  describe ".sign_for" do
     it "returns correct zodiac sign" do
-      Finder.sign_for(:month => 9, :day => 27).should == I18n.t('zodiac.libra')
+      subject.sign_for(:month => 9, :day => 27).should == I18n.t('zodiac.libra')
     end
   end
 end
