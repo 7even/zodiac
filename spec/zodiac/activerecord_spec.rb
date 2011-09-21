74a08513ede1162699b0efa33313df093483aa6f
Vsevolod Romashov <7@7vn.ru>
2011-09-21 23:49:20 +0400

    refactored a Zodiac::ActiveRecord spec

--- spec/zodiac/activerecord_spec.rb
+++ spec/zodiac/activerecord_spec.rb
@@ -6,36 +6,34 @@ class Klass < Struct.new(:dob)
   include Zodiac::ActiveRecord
 end
 
-module Zodiac
-  describe ActiveRecord do
-    describe "class methods" do
-      describe ".zodiac_reader" do
-        it "sets @date_for_zodiac with the given value" do
-          Klass.zodiac_reader :dob
-          Klass.date_for_zodiac.should == :dob
-        end
+describe Zodiac::ActiveRecord do
+  before(:each) do
+    Klass.zodiac_reader :dob
+  end
+  
+  describe "class methods" do
+    describe ".zodiac_reader" do
+      it "sets @date_for_zodiac with the given value" do
+        Klass.date_for_zodiac.should == :dob
       end
-      
-      describe ".find_by_zodiac_sign"
+    end
+  end
+  
+  describe "instance_methods" do
+    before(:each) do
+      dob = Time.gm(1967, 2, 20)
+      @object = Klass.new(dob)
     end
     
-    describe "instance_methods" do
-      before(:each) do
-        Klass.zodiac_reader :dob
-        dob = Time.gm(1967, 2, 20)
-        @object = Klass.new(dob)
-      end
-      
-      describe "#zodiac_sign" do
-        it "returns a correct zodiac sign based on date_for_zodiac attribute" do
-          @object.zodiac_sign.should == I18n.t('zodiac.pisces')
-        end
+    describe "#zodiac_sign" do
+      it "returns a correct zodiac sign based on date_for_zodiac attribute" do
+        @object.zodiac_sign.should == I18n.t('zodiac.pisces')
       end
     end
-    
-    it "is included into ActiveRecord::Base class" do
-      class ARClass < ::ActiveRecord::Base; end
-      ARClass.included_modules.should include(Zodiac::ActiveRecord)
-    end
+  end
+  
+  it "is included into ActiveRecord::Base class" do
+    class ARClass < ::ActiveRecord::Base; end
+    ARClass.included_modules.should include(Zodiac::ActiveRecord)
   end
 end
