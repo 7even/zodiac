6864aa2c8626af5767368309e346e773aeea67a3
Vsevolod Romashov <7@7vn.ru>
2011-09-24 03:12:16 +0400

    added ActiveRecord scopes

--- lib/zodiac/activerecord.rb
+++ lib/zodiac/activerecord.rb
@@ -16,9 +16,27 @@ module Zodiac
         @date_for_zodiac = dob_attribute
         
         # if the migration was applied, we should update the sign attribute before each save
+        # and define some scopes
         if self.column_names.include?('zodiac_sign_id')
           self.before_save do |object|
             object.zodiac_sign_id = object.send(dob_attribute).try(:zodiac_sign_id)
+          end
+          
+          # Person.by_zodiac(7 || :libra) == Person.where(:zodiac_sign_id => 7)
+          scope :by_zodiac, lambda { |sign|
+            case sign
+            when Symbol
+              where(:zodiac_sign_id => Zodiac::Finder::SIGN_IDS[sign])
+            when Fixnum
+              where(:zodiac_sign_id => sign)
+            else
+              raise ArgumentError, "Invalid attribute type #{sign.class} for #{self}.by_zodiac"
+            end
+          }
+          
+          # Person.gemini == Person.by_zodiac(3)
+          Zodiac::Finder::SIGN_IDS.each do |symbol, integer|
+            scope symbol, by_zodiac(integer)
           end
         end
       end
--- spec/zodiac/activerecord_spec.rb
+++ spec/zodiac/activerecord_spec.rb
@@ -2,10 +2,48 @@ require 'spec_helper'
 
 describe Zodiac::ActiveRecord do
   describe "class methods" do
+    before(:each) do
+      @gemini1 = Person.create :dob => Time.gm(1982, 5, 27)
+      @gemini2 = Person.create :dob => Time.gm(1987, 5, 29)
+      @libra   = Person.create :dob => Time.gm(1984, 9, 27)
+    end
+    
     describe ".zodiac_reader" do
       it "sets @date_for_zodiac with the given value" do
         Person.date_for_zodiac.should == :dob
       end
+    end
+    
+    describe ".by_zodiac" do
+      context "with an integer argument" do
+        it "returns a scope with objects belonging to a given sign" do
+          gemini = Person.by_zodiac(3)
+          gemini.should include(@gemini1)
+          gemini.should include(@gemini2)
+          gemini.should_not include(@libra)
+        end
+      end
+      
+      context "with a symbol argument" do
+        it "returns a scope with objects belonging to a given sign" do
+          libra = Person.by_zodiac(:libra)
+          libra.should_not include(@gemini1)
+          libra.should_not include(@gemini2)
+          libra.should include(@libra)
+        end
+      end
+    end
+    
+    describe "named zodiac scopes (like .libra)" do
+      it "call .by_zodiac with a given sign and return its result" do
+        Person.libra.should_not include(@gemini1)
+        Person.libra.should_not include(@gemini2)
+        Person.libra.should include(@libra)
+      end
+    end  
+    
+    after(:each) do
+      Person.delete_all
     end
   end
   
