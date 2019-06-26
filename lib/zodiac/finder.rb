f3cfbc06d530820807aa533204711b5fdb10ff2c
Maxim Romanov <maxim.romanov@jetrockets.ru>
2019-06-26 17:24:27 +0300

    Fixed dates for zodiac signs

--- lib/zodiac/finder.rb
+++ lib/zodiac/finder.rb
@@ -1,34 +1,34 @@
 module Zodiac
   module Finder
     YEAR = 2012
-    
+
     class << self
       def date_for(month, day)
         DateTime.new(YEAR, month, day)
       end
-      
+
       def range_for(month_start, day_start, month_end, day_end)
         start, ending = date_for(month_start, day_start), date_for(month_end, day_end)
         SimpleRange.new(start, ending)
       end
     end
-    
+
     RANGES = {
-      range_for(1,  1,  1,  20) => :capricorn,
-      range_for(1,  21, 2,  19) => :aquarius,
-      range_for(2,  20, 3,  20) => :pisces,
-      range_for(3,  21, 4,  20) => :aries,
-      range_for(4,  21, 5,  21) => :taurus,
-      range_for(5,  22, 6,  21) => :gemini,
-      range_for(6,  22, 7,  22) => :cancer,
-      range_for(7,  23, 8,  21) => :leo,
-      range_for(8,  22, 9,  23) => :virgo,
-      range_for(9,  24, 10, 23) => :libra,
-      range_for(10, 24, 11, 22) => :scorpio,
-      range_for(11, 23, 12, 22) => :sagittarius,
-      range_for(12, 23, 12, 31) => :capricorn
+      range_for(1,  1,  1,  19) => :capricorn,
+      range_for(1,  20, 2,  18) => :aquarius,
+      range_for(2,  19, 3,  20) => :pisces,
+      range_for(3,  21, 4,  19) => :aries,
+      range_for(4,  20, 5,  20) => :taurus,
+      range_for(5,  21, 6,  20) => :gemini,
+      range_for(6,  21, 7,  22) => :cancer,
+      range_for(7,  23, 8,  22) => :leo,
+      range_for(8,  23, 9,  22) => :virgo,
+      range_for(9,  23, 10, 22) => :libra,
+      range_for(10, 23, 11, 21) => :scorpio,
+      range_for(11, 22, 12, 21) => :sagittarius,
+      range_for(12, 22, 12, 31) => :capricorn
     }
-    
+
     SIGN_IDS = {
       aries:       1,
       taurus:      2,
@@ -43,23 +43,23 @@ module Zodiac
       aquarius:    11,
       pisces:      12
     }
-    
+
     class << self
       def sign_for(date)
         I18n.t!("zodiac.#{self.sign_symbol_for date}")
       end
-      
+
       def sign_id_for(date)
         SIGN_IDS[self.sign_symbol_for date]
       end
-      
+
       def sign_symbol_for(date)
         RANGES.each do |range, sign|
           if range.days.cover? date_for(date[:month], date[:day])
             return sign
           end
         end
-        
+
         raise ArgumentError
       end
     end
